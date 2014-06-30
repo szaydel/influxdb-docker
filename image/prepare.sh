439b3d7d1cb1194e55beff6fa27f2b3c6183ef90
Sam Zaydel <szaydel@gmail.com>
2014-06-30 07:07:39 -0700

    Satisfy dependencies for installation of influx-db.

--- image/prepare.sh
+++ image/prepare.sh
@@ -36,6 +36,9 @@ $minimal_apt_get_install apt-transport-https ca-certificates
 ## Install add-apt-repository
 $minimal_apt_get_install software-properties-common
 
+## Install curl package
+$minimal_apt_get_install curl
+
 ## Upgrade all packages.
 apt-get dist-upgrade -y --no-install-recommends
 
